56235409ed9af9ef17bbb3a751c08d735ea8ca41
Merit Student <student@meac.local>
2018-08-28 14:34:10 -0600

    i did it!

--- unit1/data-types.py
+++ unit1/data-types.py
@@ -1,4 +1,6 @@
 """
+
+
 Project:     Data Types Notes
 Author:      Mr. Buckley
 Last update: 8/25/2018
@@ -16,23 +18,35 @@ multiline comment
 
 # *** DATA TYPE: INTEGER ***
 # TODO: An integer number (no decimal)
-
+integer = 5
+print (integer)
+print(type(integer))
 
 # *** DATA TYPE: FLOAT ***
 # TODO: A decimal number
-
+decimal = 4.385
+print(type(decimal))
 
 # *** DATA TYPE: STRING ***
 # TODO: A string of characters enclosed in quotes
-
+word = "These are my words"
+print(word)
+print(type(word))
 
 # *** TYPE CASTING ***
 # This converts one type to another
 
 # TODO: Cast float to int
-
+decimal = 3.1415974627890
+dec_to_int = int(decimal)
+print(dec_to_int)
 # TODO: Cast int to string
+number = 4
+print(int(number) + 4)
 
 # TODO: Cast number string to int
 
 # TODO: Input demo (str to float)
+print("Give me a freaking decimal and I'll add 1 to it")
+number = input(float)
+print(number + 1)
